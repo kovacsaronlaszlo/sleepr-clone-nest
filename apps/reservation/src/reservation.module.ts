451a5bdc8ddeabfb100c32b8f2cfc4f0dd1dbd89
kovacsaronlaszlo <k.aron.laszlo@gmail.com>
2024-11-21 15:36:12 +0100

    created reservation CRUD using nest cli

---
+++ apps/reservation/src/reservation.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { ReservationService } from './reservation.service';
+import { ReservationController } from './reservation.controller';
+import { DatabaseModule } from '@app/common';
+import { ReservationRepository } from './resevation.repository';
+import {
+  ReservationDocument,
+  ReservationSchema,
+} from './models/reservation.schema';
+
+@Module({
+  imports: [
+    DatabaseModule,
+    DatabaseModule.forFeature([
+      { name: ReservationDocument.name, schema: ReservationSchema },
+    ]),
+  ],
+  controllers: [ReservationController],
+  providers: [ReservationService, ReservationRepository],
+})
+export class ReservationModule {}
