044c8132bee40dff980bf3c1242ae8654271bb7a
kovacsaronlaszlo <k.aron.laszlo@gmail.com>
2024-11-24 15:21:25 +0100

    install and set password and jwt packages, refactor configService for apps and crate .env file each apps

--- apps/reservation/src/reservation.module.ts
+++ apps/reservation/src/reservation.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 import { ReservationService } from './reservation.service';
 import { ReservationController } from './reservation.controller';
 import { DatabaseModule, LoggerModule } from '@app/common';
@@ -15,6 +17,13 @@ import {
       { name: ReservationDocument.name, schema: ReservationSchema },
     ]),
     LoggerModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required(),
+        PORT: Joi.number().required(),
+      }),
+    }),
   ],
   controllers: [ReservationController],
   providers: [ReservationService, ReservationRepository],
