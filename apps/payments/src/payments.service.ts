2fddd6521b0285fe91ba51b72d2eb471b8e46c01
kovacsaronlaszlo <k.aron.laszlo@gmail.com>
2024-11-25 18:09:21 +0100

    create payments app | install and set stripe | registered to stripe | create Dockerfile | creatChager service and controller

---
+++ apps/payments/src/payments.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import Stripe from 'stripe';
+import { CreateChargeDto } from './dto/create-charge.dto';
+
+@Injectable()
+export class PaymentsService {
+  private readonly stripe: Stripe;
+
+  constructor(private readonly configService: ConfigService) {
+    const stripeSecretKey = this.configService.get<string>('STRIPE_SECRET_KEY');
+    if (!stripeSecretKey) {
+      throw new Error('Stripe secret key is not defined in configuration');
+    }
+
+    this.stripe = new Stripe(stripeSecretKey, {
+      apiVersion: '2024-11-20.acacia',
+    });
+  }
+
+  async createCharge({ card, amount }: CreateChargeDto) {
+    const paymentMethod = await this.stripe.paymentMethods.create({
+      type: 'card',
+      card,
+    });
+
+    const paymentIntent = await this.stripe.paymentIntents.create({
+      payment_method: paymentMethod.id,
+      amount: amount * 100,
+      confirm: true,
+      payment_method_types: ['card'],
+      currency: 'usd',
+    });
+
+    return paymentIntent;
+  }
+}
